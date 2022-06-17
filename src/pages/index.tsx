0b253b0daf605b417633f0c8079b9fd871955663
Hoang Truong <tvhoang91@gmail.com>
2022-06-17 17:35:03 +0700

    formatting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from 'next';
+import Head from 'next/head';
 
-import Counter from '../features/counter/Counter'
-import styles from '../styles/Home.module.css'
+import Counter from '../features/counter/Counter';
+import styles from '../styles/Home.module.css';
 
 const IndexPage: NextPage = () => {
   return (
@@ -19,45 +19,25 @@ const IndexPage: NextPage = () => {
         </p>
         <span>
           <span>Learn </span>
-          <a
-            className={styles.link}
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className={styles.link} href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
             React
           </a>
           <span>, </span>
-          <a
-            className={styles.link}
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className={styles.link} href="https://redux.js.org/" target="_blank" rel="noopener noreferrer">
             Redux
           </a>
           <span>, </span>
-          <a
-            className={styles.link}
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className={styles.link} href="https://redux-toolkit.js.org/" target="_blank" rel="noopener noreferrer">
             Redux Toolkit
           </a>
           ,<span> and </span>
-          <a
-            className={styles.link}
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className={styles.link} href="https://react-redux.js.org/" target="_blank" rel="noopener noreferrer">
             React Redux
           </a>
         </span>
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
